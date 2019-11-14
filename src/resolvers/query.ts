f3059b8eddb2ef672690d84beadafcf8e5c20b73
AlexisNarvaez <alexiszed19@gmail.com>
2019-11-13 19:02:20 -0600

    Lista de cursos

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,25 +1,30 @@
-import { IResolvers } from 'graphql-tools';
-import { database } from '../data/data.store';
+import { IResolvers } from "graphql-tools";
+import { database } from "../data/data.store";
 
 const query: IResolvers = {
-    Query: {
-        estudiantes(): any {
-            return database.estudiantes;
-        },
-        estudiante(__: void, { id }): any {
-            const resultado = database.estudiantes.filter( estudiante => estudiante.id === id)[0];
+  Query: {
+    estudiantes(): any {
+      return database.estudiantes;
+    },
+    estudiante(__: void, { id }): any {
+      const resultado = database.estudiantes.filter(
+        estudiante => estudiante.id === id
+      )[0];
 
-            if ( resultado === undefined) {
-                return {
-                    id: '-1',
-                    name: `No se ha encontrado el estudiante con el ID ${id}`,
-                    email: '',
-                    courses: []
-                }
-            }
-            return resultado;
-        }
+      if (resultado === undefined) {
+        return {
+          id: "-1",
+          name: `No se ha encontrado el estudiante con el ID ${id}`,
+          email: "",
+          courses: []
+        };
+      }
+      return resultado;
+    },
+    cursos(): any {
+      return database.cursos;
     }
-}
+  }
+};
 
 export default query;
